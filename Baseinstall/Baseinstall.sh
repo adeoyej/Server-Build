50af1bac3f9ec751a7e9b4d12340006067ff742d
root <root@ip-10-0-4-35.ec2.internal>
2018-11-27 20:10:46 +0000

    added instructions

--- Baseinstall/Baseinstall.sh
+++ Baseinstall/Baseinstall.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#installing update
 yum -y update
+#adding a user called and password for AdminU1
 adduser AdminU1
 passwd AdminU1 
-echo 'AdminU1 ALL=(ALL:ALL) ALL' >> /etc/sudoers 
+#adding user to sudoer file
+echo 'AdminU1 ALL=(ALL:ALL) ALL' >> /etc/sudoers
+#generating an ssh key 
 ssh-keygen -f /home/AdminU1/.ssh
 
