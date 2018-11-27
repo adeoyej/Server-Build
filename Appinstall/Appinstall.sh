db132e80467ccb0e7e9d3e6de5e8454fa2d7b2f6
root <root@ip-10-0-4-35.ec2.internal>
2018-11-27 19:48:53 +0000

    added instructions

--- Appinstall/Appinstall.sh
+++ Appinstall/Appinstall.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+#installing java jdk
 yum -y install java-1.8.0-openjdk-devel
+#installing dependencies
 yum groupinstall -y "development tools" 
 yum install -y libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
+# chaning directory to usr/src
 cd /usr/src
+#downloading the python file
 wget http://python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
+# extracting the file
 tar xf Python-3.6.4.tar.xz
+#changing directory 
 cd Python-3.6.4
+#enabling optimizations 
 ./configure --enable-optimizations
+#installs python 3.6
 make altinstall
 exit
