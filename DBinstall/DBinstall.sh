3b728d36e600d0fa48980c661a36e8d5ca16388d
root <root@ip-10-0-4-35.ec2.internal>
2018-11-27 22:51:00 +0000

    added instructions

--- DBinstall/DBinstall.sh
+++ DBinstall/DBinstall.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#getting mysql repo
 wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 md5sum mysql57-community-release-el7-9.noarch.rpm
 rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+#installing mysql-server
 yum install mysql-server
+#starting the mysql server
 systemctl start mysqld
+#chekcing the status
 systemctl status mysqld
 mysqladmin -u root -p version
